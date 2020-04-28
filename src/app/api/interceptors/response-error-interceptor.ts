2bad3483f7306b98c1178aa7b0a64a5b8fee745a
Vlad <Vladyslav@qooling.com>
2020-04-28 20:48:32 +0300

    Add error handling for 'SSL/TLS Certificate error'

--- src/app/api/interceptors/response-error-interceptor.ts
+++ src/app/api/interceptors/response-error-interceptor.ts
@@ -12,6 +12,7 @@ export class ResponseErrorInterceptor implements HttpInterceptor {
   private config: object = {
     timeOut: 10000,
     extendedTimeOut: 5000,
+    enableHtml: true,
     tapToDismiss: false,
     closeButton: true,
     progressBar: true
@@ -35,11 +36,33 @@ export class ResponseErrorInterceptor implements HttpInterceptor {
   }
 
   private showError(err: HttpErrorResponse): void {
-    if (err.error.hasOwnProperty('title') && err.error.hasOwnProperty('description') && err.error.hasOwnProperty('status')) {
-      this.toasterService.error(`${err.error.description}`, `${err.error.title} - Status: (${err.error.status})`, this.config);
-    } else {
-      this.toasterService.error(`url: ${err.url}`, `${err.error.error} - Status: (${err.error.status})`, this.config);
+    const errorCase = this.getErrorCase(err);
+
+    switch (errorCase) {
+      case '0':
+        this.toasterService.error(`Can't get access to endpoint due using self-written certificate.
+        Make sure you go to AWS Beanstalk and either click allow to Trust this web-site or disable antivirus checking.
+        <a class="btn btn-primary" href='${err.url}' target="_blank">Open AWS</a>.`,
+          `Https Certificate Error`, this.config);
+        break;
+      case '1':
+        this.toasterService.error(`${err.error.description}`, `${err.error.title} - Status: (${err.error.status})`, this.config);
+        break;
+      case '2':
+        this.toasterService.error(`url: ${err.url}`, `${err.error.error} - Status: (${err.error.status})`, this.config);
+        break;
+      default:
+        this.toasterService.error('An error occurred!', '', this.config);
     }
   }
 
+  public getErrorCase(err: HttpErrorResponse): string {
+    if (err.status === 0 && err.statusText === 'Unknown Error') {
+      return '0';
+    } else if (err.error.hasOwnProperty('title') && err.error.hasOwnProperty('description') && err.error.hasOwnProperty('status')) {
+      return '1';
+    }
+
+    return '2';
+  }
 }
