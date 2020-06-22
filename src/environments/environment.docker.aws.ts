0e179eac787b37a4593a5ca37d389944842fbfcd
Vlad <vladyslav.zr@gmail.com>
2020-06-22 18:42:32 +0300

    Add new backend url to interact with

--- src/environments/environment.docker.aws.ts
+++ src/environments/environment.docker.aws.ts
@@ -2,5 +2,5 @@ export const environment = {
   name: 'Amazon based on Docker',
   description: 'Environment for Amazon backend to access to backend Docker container internally',
   production: true,
-  apiUrl: 'http://awseb-e-4-awsebloa-ioo0l8cvyfa5-60284753.us-east-2.elb.amazonaws.com:5001/backend'
+  apiUrl: 'http://application-1698499636.us-east-2.elb.amazonaws.com/backend'
 };
